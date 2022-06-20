c63143e744e5053d994b4cbc71310a5df19c1e14
Rishabh Pratap Singh <rpratapsingh333@gmail.com>
2022-06-20 20:48:43 +0530

    pushed update code

---
+++ build/precache-manifest.8541cccb0bea53911f1a78a514ea309a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "268e276754a141ec6edca0dbf6b90573",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ce65e4967ff2ee6e1ee",
+    "url": "/static/css/2.3fc42fd8.chunk.css"
+  },
+  {
+    "revision": "10084ee8f22dfda25b93",
+    "url": "/static/css/main.9a174636.chunk.css"
+  },
+  {
+    "revision": "7ce65e4967ff2ee6e1ee",
+    "url": "/static/js/2.c96cc233.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.c96cc233.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10084ee8f22dfda25b93",
+    "url": "/static/js/main.3fd3f46c.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
